7999088b426e44d41f98255396d06afbb97e91e2
JonasBirgersson <jonas.birgersson@entero.com>
2015-01-23 11:16:49 -0700

    Updated the destination

--- server/src/main/java/com/outdoorlog/model/Destination.java
+++ server/src/main/java/com/outdoorlog/model/Destination.java
@@ -1,4 +1,9 @@
 package com.outdoorlog.model;
 
 public class Destination {
+
+    private String name;
+    private String description;
+    private double latitude;
+    private double longitude;
 }
